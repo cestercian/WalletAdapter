d5870e939c7aa49e81b31f591ec28e5af29b642c
Cestercian <yashafaid@gmail.com>
2025-04-20 01:34:28 +0530

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
     WalletDisconnectButton,
-    WalletMultiButton
+    WalletMultiButton,
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
-
-// Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 import './App.css'
 import Airdrop from "./Airdrop.jsx";
@@ -20,10 +18,12 @@ function App() {
 
   return (
     <>
-        <ConnectionProvider endpoint={""}>
+        <ConnectionProvider endpoint={"https://solfaucet.com"}>
             <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
-                    <>HI</>
+                    <WalletMultiButton/>
+                    <WalletDisconnectButton/>
+                    <div>HEHE</div>
                     <Airdrop></Airdrop>
                 </WalletModalProvider>
             </WalletProvider>
