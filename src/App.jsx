85df1eda8f6883b4ed5bc6f5c1746d13a75d013c
Cestercian <yashafaid@gmail.com>
2025-04-20 00:50:30 +0530

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
 import './App.css'
 
 function App() {
-  const [, ] = useState()
+
+
 
   return (
     <>
-        <h1 style={{ color: 'white' }}></h1>
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
     </>
   )
 }
