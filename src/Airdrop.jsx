09c545f9a0667cadecc2e13c20369c6ec97942ec
Cestercian <yashafaid@gmail.com>
2025-04-20 01:02:20 +0530

    v1.0

---
+++ src/Airdrop.jsx
@@ -0,0 +1,8 @@
+export default function Airdrop() {
+    return (
+        <>
+            <input type={"text"} placeholder={"Amount"} ></input>
+            <button>Send Airdrop</button>
+        </>
+    )
+}
